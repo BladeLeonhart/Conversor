03771372568d862dd6972f5c7907bcff3079800a
Luis <luisxinzo78@hotmail.com>
2019-05-09 09:32:13 +0200

    Clase Lonxitude

--- src/conversor/Lonxitude.java
+++ src/conversor/Lonxitude.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package conversor;
 
-/**
- *
- * @author DAM117
- */
 public class Lonxitude {
     
+    private double Km;
+    private double Millas;
+    private double Pulgadas;
+    
+    
+    public String KilometrosMillas(double Km){
+        double Millas;
+        Millas = Km * 0.621;
+        return Km+"Son "+Millas;
+    }
+    
+    public String MillasKilometros(double Millas){
+        double Km;
+        Km = Millas * 1.61;
+        return Millas+"Son "+Km;    
+    }
+    
+    public String MillasPulgadas(double Millas){
+        double Pulgadas;
+        Pulgadas = Millas * 63360;
+        return Millas+"Son "+Pulgadas;    
+    }
+    
+    public String PulgadasMillas(double Pulgadas){
+        double Millas;
+        Millas = Pulgadas * 0.00001578;
+        return Pulgadas+"Son "+Millas;    
+    }
+    
+    
+    public void Mostrar(int num){ 
+        System.out.println("1. Transformar Kms a Millas\n"
+                + "2. Transformar Millas a Kms"
+                + "3. Transformar Millas a Pulgadas"
+                + "4. Transformar Pulgadas a Millas"
+                + "0. Volver ó menú anterior");
+        switch(num){
+            case 1: KilometrosMillas(Km);
+            break;
+            case 2: MillasKilometros(Millas);
+            break;
+            case 3: MillasPulgadas(Millas);
+            break;
+            case 4: PulgadasMillas(Pulgadas);
+            break;
+        }
+        System.out.println("Fin xD");
+    }
+    
 }
+
+
