169cfa6e30f3e2613e20958f29c15583250b704f
Pablo Negreira <ronepablo10@gmail.com>
2019-05-09 09:12:51 +0200

    Clase Potencia

--- src/conversor/Potencia.java
+++ src/conversor/Potencia.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package conversor;
 
-/**
- *
- * @author DAM117
- */
 public class Potencia {
-    
+
+    private double KW;
+    private double CV;
+    private double FtLB;
+
+    private void ConversionKWaCV(double KW) {
+        double CV;
+        CV = KW * 1.36;
+        System.out.println(KW + "KW a CV son " + CV);
+    }
+
+    private void ConversionCVaKW(double CV) {
+        double KW;
+        KW = CV * 0.736;
+        System.out.println(CV + "CV a KW son " + KW);
+    }
+
+    private void ConversionKWaFTLB(double KW) {
+        double FTLB;
+        FTLB = KW * 737.562149;
+        System.out.println(KW + "KW a FTLB son " + FTLB);
+    }
+
+    private void ConversionFTLBaKW(double FTLB) {
+        double KW;
+        KW = FTLB / 737.562149;
+        System.out.println(FTLB + "FTLB a KW son " + KW);
+    }
+
 }
